333220fe17c6b3f3248cfccd1e6ed9ccd72b9b32
fadleyy <user.fadli3001@gmail.com>
2023-10-17 21:37:49 +0700

    menyambungkan routers.js ke server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
 
+// parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// panggil routers
+
+const routers = require("./routers");
+routers(app);
 
 app.listen(port, () => {
   console.log(`server berjalan di https://localhost:${port}`);
