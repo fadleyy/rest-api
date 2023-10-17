049d20fafb16fb16007f6b5e5800d027773b83f7
fadleyy <user.fadli3001@gmail.com>
2023-10-17 21:07:30 +0700

    menambahkan koneksi.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 4000;
+const db = require("./konesksi");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`server berjalan di https://localhost:${port}`);
+});
