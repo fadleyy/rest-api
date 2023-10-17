8a22c3da83210b86a8004cfdbd68a99a47de9894
fadleyy <user.fadli3001@gmail.com>
2023-10-17 21:23:02 +0700

    merubah isi server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
-const db = require("./konesksi");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
