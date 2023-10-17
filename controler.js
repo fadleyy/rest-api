3bf7841f451ccddb40472121940f815f93b92b19
fadleyy <user.fadli3001@gmail.com>
2023-10-17 21:42:20 +0700

    mengfix controler.js

--- controler.js
+++ controler.js
@@ -4,5 +4,5 @@ const response = require("./response");
 const conn = require("./konesksi");
 
 exports.index = function (req, res) {
-  response.ok("apklikasi api aman");
+  response.ok("apklikasi api aman", res);
 };
